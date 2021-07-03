495fc59a48aee6ba8d720dfff65cf9b6c7ede88a
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-02 21:37:29 -0700

    Note velocity parsing for each frame.

--- src/midi.py
+++ src/midi.py
@@ -18,12 +18,41 @@
 #
 
 import mido
+import numpy as np
 
 
-def parse_midi(path):
-    messages = []
+def parse_midi(path, fps):
+    msgs = []
     curr_time = 0
     for msg in mido.MidiFile(path):
-        curr_time += msg.time
-        messages.append((curr_time, msg))
-    return messages
+        curr_time += msg.time*fps
+        msgs.append((int(curr_time), msg))
+
+    return msgs
+
+
+def note_vels(msgs, fps):
+    vels = np.zeros((88,), dtype=np.float32)
+    starts = np.zeros((88,), dtype=np.float32)
+    frame = 0
+
+    fac = 0.75 ** (1/fps)
+    thres = 0.01
+    msgs = msgs.copy()
+
+    while True:
+        if frame >= msgs[0][0]:
+            msg = msgs.pop(0)[1]
+            if msg.type in ("note_on", "note_off"):
+                note = msg.note - 21
+                vel = 0 if msg.type == "note_off" else msg.velocity
+                vels[note] = vel
+                starts[note] = frame
+
+        vels = vels * fac
+        vels[vels<thres] = 0
+
+        yield (vels, starts)
+
+        if len(msgs) == 0 and not np.any(vels):
+            break
